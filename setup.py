ff9d2b017c806ee7a5eb8ec593c6149c9738b230
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2020-06-17 20:14:54 +0200

    version 0.104

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="pytigon-batteries",
-    version="0.100",
+    version="0.104",
     description="Pytigon library",
     author="Sławomir Chołaj",
     author_email="slawomir.cholaj@gmail.com",
@@ -46,4 +46,3 @@ setup(
     ],
     python_requires=">=3",
 )
-
